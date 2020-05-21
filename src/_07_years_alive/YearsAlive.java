cd540edee71944368537598939c70a8518a75832
root <root@7a91fccd32a9>
2020-05-21 20:25:48 +0000

    Shiny Objects

---
+++ src/_07_years_alive/YearsAlive.java
@@ -0,0 +1,15 @@
+package _07_years_alive;
+
+public class YearsAlive {
+ public static void main(String[] args) {
+
+	 for(int years = 2007; years<=2020; years++) {
+    System.out.println(years);
+		 
+		 
+	 }
+	
+}
+	
+
+}
--- src/_08_shiny_objects/ShinyObjects.java
+++ src/_08_shiny_objects/ShinyObjects.java
@@ -8,19 +8,21 @@ import java.applet.AudioClip;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 		// 2. Ask the user how many shiny objects they want
-
+   JOptionPane.showInputDialog("How many shiny items do you want?");
 		// 3. Play the sound that many times
-
+   if 
 		// 1. Call the method below
 
 	}
 
 	public static void playMisterZee() {
+		System.out.println("play mister zee");
 		try {
 			AudioClip sound = JApplet.newAudioClip(ShinyObjects.class.getResource("shiny-objects.wav"));
 			sound.play();
