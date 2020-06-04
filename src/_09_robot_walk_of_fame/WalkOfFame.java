0224594103f53f489eeadf800eaffef183ab644b
root <root@51ada19cc187>
2020-06-04 20:32:07 +0000

    Walk of fame

--- src/_09_robot_walk_of_fame/WalkOfFame.java
+++ src/_09_robot_walk_of_fame/WalkOfFame.java
@@ -4,16 +4,50 @@
 
 package _09_robot_walk_of_fame;
 
+import java.awt.MouseInfo;
+import java.awt.Point;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
-	public static void main(String[] args) {
-		Robot rob = new Robot();
+	public static void main(String[] args) { 
+	
 		// 1. Set the X position of the robot so that it starts on the left. 
-		// You also need to show the robot to see the result of this line.
-
+	
+				int x  = 100;
+				int y  = 100;
+				int move = 200;
+		
+	// You also need to show the robot to see the result of this lilaine.
+		
 		// 2. Make the robot draw a star shape. Hint: 144.
-
+				for(int i = 0; i<30; i++) {
+					Robot hi = new Robot();
+					hi.setX(x);//100, 150
+					hi.setY(y);
+					hi.setRandomPenColor();		
+					hi.penDown();
+					
+				hi.setSpeed(500);
+					for(int j=0; j< 10; j++) {
+              hi.move(30);
+              hi.turn(144);
+					}
+              hi.penUp();
+  			hi.move(move);
+  			
+  			x = x + 50;  // x += 50;         
+  			if( x >= 500) {
+				y += 200;
+				x = 100;
+				move += 200;
+				
+  			}
+		}
+						
+			
+			
+				
 		// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
@@ -23,6 +57,7 @@ public class WalkOfFame {
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
+		//I remember that we did the main method last week rather sure YEHHH
 
 	}
 
